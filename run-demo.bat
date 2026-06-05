ca7c916bb0690f48922408efe9c8e13a38f66146
Andre Stubbe <andrestubbe@proton.me>
2026-06-05 13:58:39 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,11 +1,10 @@
-﻿@echo off
-chcp 65001 >nul
+@echo off
     echo âŒ Main install failed!
     pause 
     exit /b 
 )
 echo Running Console Demo...
 cd examples\Demo
-call mvn compile exec:exec -q
+call mvn compile exec:exec
 cd ..\..
 pause
